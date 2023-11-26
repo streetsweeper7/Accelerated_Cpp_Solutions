248c7c8e3cf0f562fc54ad767aa51f6e88b6aff9
root <root@DESKTOP-6I5TFMT>
2023-11-26 11:45:32 +0000

    Ch0_4 solution

---
+++ source/chapter0/0_4.cpp
@@ -0,0 +1,8 @@
+// This program writes the "Hello, world!" program as its output
+#include <iostream>
+
+int main() {
+
+    std::cout << "#include <iostream>\n\nint main(){\n\tstd::cout << \"Hello, world!\" << std::endl;\n\treturn 0;\n}" << std::endl;
+    return 0;
+}

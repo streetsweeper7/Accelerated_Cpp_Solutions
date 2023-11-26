f67af9ee3a32c633e72030e0519d127238d6b36c
root <root@DESKTOP-6I5TFMT>
2023-11-26 12:19:29 +0000

    Ch0_5_to_10 solutions

---
+++ source/chapter0/0_7.cpp
@@ -0,0 +1,9 @@
+// This program writes the "Hello, world!" program as its output
+#include <iostream>
+
+int main() {
+    /* This is a comment that extends over several lines
+        because it uses /* and */ as its starting and ending delimiters */
+    std::cout << "Does this work?" << std::endl;
+    return 0;
+}
---
+++ source/chapter0/non_coding_qstns.cpp
@@ -0,0 +1,2 @@
+//0_1: 3 + 4; calculates the value of 3 + 4 but discards the result. It has no side effect
+            // on the program no the operands as they are only int literals
